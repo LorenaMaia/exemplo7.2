33bdfd1bbe1cab06a57974b7ceed504fcd583006
Lorena <lorenamaia329@gmail.com>
2019-06-09 03:14:48 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String resposta;
+   
+       
+            Console.WriteLine(" O céu é azul?");
+            resposta = Console.ReadLine();
+
+            while (resposta != "sim") {
+                Console.WriteLine(" O céu é azul?");
+                resposta = Console.ReadLine();
+            }
+            Console.WriteLine(" Você acertou! ");
+            Environment.Exit(0);
+            
+            }
+            
+
+
+        }
+    }
+
